ca99abdbeb41fcc3f05057e781950adf096deda2
vinodhmahendra <vinodh.mahendra@gmail.com>
2021-09-21 14:43:56 +0530

    initial commit

---
+++ hello-world-rest-api/src/main/java/com/simplilearn/workshop/resources/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.simplilearn.workshop.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/")
+	public String helloWorld() {
+		return "Hello World! Spring Boot";
+	}
+}
